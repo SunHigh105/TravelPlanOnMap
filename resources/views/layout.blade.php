ec943cb4e05891337d53b0373d1b1e9f8c5fa38f
Ayaka Hidaka <ayaka@AyakanoMacBook-Pro.local>
2019-08-07 20:26:28 +0900

    delete vue script from layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,6 @@
 </head>
 <body>
   @yield('content')
-  <script src="https://cdn.jsdelivr.net/npm/vue"></script>
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <script src="{{ asset('js/app.js')}}"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
