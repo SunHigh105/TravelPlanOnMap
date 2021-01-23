62c67e0069fea6321c5422052ae031deed8f8fc2
Ayaka Hidaka <ayakahidaka@AyakanoMacBook-Pro.local>
2021-01-23 15:45:28 +0900

    layout.blade.php削除

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,6 +1,15 @@
-@extends('layout')
-
-@section('content')
-<div id="app" v-cloak>
-    <main-page></main-page>
-</div>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>TravelPlanMap</title>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <div id="app" v-cloak></div>
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+  <script src="{{ asset('js/app.js')}}"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</body>
+</html>
